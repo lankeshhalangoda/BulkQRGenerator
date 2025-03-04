047a23996c5656b5f70d9e8b83fa5b05f7b8114e
lankeshhalangoda <lankeshhalangoda@gmail.com>
2025-03-04 15:39:31 +0530

    update v1.2 - favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,27 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from 'next';
+import Head from 'next/head';
+import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Emojot | Bulk QR Generator',
-  description: 'Emojot\'s Bulk QR Generator is an internal tool designed for efficiently generating multiple QR codes in bulk for various use cases.',
+  description: "Emojot's Bulk QR Generator is an internal tool designed for efficiently generating multiple QR codes in bulk for various use cases.",
   generator: 'v0.dev',
   icons: {
     icon: '/favicon.png',
   },
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </Head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
